42516450b7635455126a4edc144280d605938e37
Christopher Liscio <chris@supermegaultragroovy.com>
2016-10-05 13:33:02 -0400

    Initial Commit

---
+++ ReactiveCocoaSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ReactiveCocoaSample
+//
+//  Created by Christopher Liscio on 10/5/16.
+//  Copyright © 2016 SuperMegaUltraGroovy, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
